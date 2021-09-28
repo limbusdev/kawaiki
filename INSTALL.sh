0ab63672e26113835ab6a7afc7cd303804a59f76
Georg Eckert <lucidtanooki@magenta.de>
2021-09-28 18:30:02 +0200

    adds two new themes: Silver and WarmNoon

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,27 +4,27 @@
 green='\033[0;32m'
 NC='\033[0m' # No Color
 
-echo " __   ___  __  __           _   __        __   __   ___  __ "
-echo "|  | /  / |  \ \ \         | | |  \      |  | |  | /  / |  |"
-echo "|  |/  /  |   \ \ \   __   | | |   \     |  | |  |/  /  |  |"
-echo "|     /   | |\ \ \ \ |  \  | | | |\ \    |  | |     /   |  |"
-echo "|     \   | |_\ \ \ \||\ \ | | | |_\ \   |  | |     \   |  |"
-echo "|  |\  \  |  __  \ \  | \ \| | |  __  \  |  | |  |\  \  |  |"
-echo "|__| \__\ |_|  \__\ \_|  \___| |_|  \__\ |__| |__| \__\ |__|"
+echo " __   ___  __  __           _   __        __   __   ___  __      __      ____    __  "
+echo "|  | /  / |  \ \ \         | | |  \      |  | |  | /  / |  |    |  \    |    \  /  / "
+echo "|  |/  /  |   \ \ \   __   | | |   \     |  | |  |/  /  |  |    |   \   | |\  \/  /  "
+echo "|     /   | |\ \ \ \ |  \  | | | |\ \    |  | |     /   |  | __ | |\ \  | | \    /   "
+echo "|     \   | |_\ \ \ \||\ \ | | | |_\ \   |  | |     \   |  ||__|| | \ \ | | /    \   "
+echo "|  |\  \  |  __  \ \  | \ \| | |  __  \  |  | |  |\  \  |  |    | |  \ \| |/  /\  \  "
+echo "|__| \__\ |_|  \__\ \_|  \___| |_|  \__\ |__| |__| \__\ |__|    |_|   \______/  \__\ "   
 
 echo ""
-echo -e "${green}             ------------------------------${NC}"
-echo -e "${green}              Kawaiki Icon Theme Installer${NC}"
-echo -e "${green}             ------------------------------${NC}"
+echo -e "${green}             --------------------------------${NC}"
+echo -e "${green}              KawaikiNX Icon Theme Installer ${NC}"
+echo -e "${green}             --------------------------------${NC}"
 echo ""
 
 # Check if executing user has root-priviliges
 ROOT_UID=0
 if [ "$UID" -ne "$ROOT_UID" ]
 then
-	echo "Kawaiki-Icon-Theme will be copied to $HOME/.icons. To make it available system-wide, run as root."
+	echo "KawaikiNX-Icon-Theme will be copied to $HOME/.icons. To make it available system-wide, run as root."
 else
-	echo "Kawaiki-Icon-Theme will be copied to /usr/share/icons. To make it available for yourself only, run as standard-user"
+	echo "KawaikiNX-Icon-Theme will be copied to /usr/share/icons. To make it available for yourself only, run as standard-user"
 fi
 
 # Ask if process should be continued
@@ -37,34 +37,12 @@ fi
 
 # this directory
 basedir=$(pwd)
-theme1="/Kawaiki-Only"
+theme1="/KawaikiNX-Only"
 theme1source=$basedir$theme1
-theme2="/Kawaiki-GoldenSunshine"
+theme2="/KawaikiNX-Silver"
 theme2source=$basedir$theme2
-theme3="/Kawaiki-HappyFrog"
+theme3="/KawaikiNX-WarmNoon"
 theme3source=$basedir$theme3
-theme4="/Kawaiki-HumanEarth"
-theme4source=$basedir$theme4
-theme5="/Kawaiki-IcePrincess"
-theme5source=$basedir$theme5
-theme6="/Kawaiki-PinkPatrick"
-theme6source=$basedir$theme6
-theme7="/Kawaiki-PurpleGem"
-theme7source=$basedir$theme7
-theme8="/Kawaiki-SummerWine"
-theme8source=$basedir$theme8
-theme9="/Kawaiki-TurquoiseBay"
-theme9source=$basedir$theme9
-theme10="/Kawaiki-WildSerengeti"
-theme10source=$basedir$theme10
-theme11="/Kawaiki-PitchBlack"
-theme11source=$basedir$theme11
-theme12="/Kawaiki-SnowWhite"
-theme12source=$basedir$theme12
-theme13="/Kawaiki-AzurOcean"
-theme13source=$basedir$theme13
-theme14="/Kawaiki-BreezyForest"
-theme14source=$basedir$theme14
 
 # copy files, depending on user-rights
 
@@ -74,33 +52,11 @@ then
     cp -r $theme1source ~/.icons/
     cp -r $theme2source ~/.icons/
     cp -r $theme3source ~/.icons/
-    cp -r $theme4source ~/.icons/
-    cp -r $theme5source ~/.icons/
-    cp -r $theme6source ~/.icons/
-    cp -r $theme7source ~/.icons/
-    cp -r $theme8source ~/.icons/
-    cp -r $theme9source ~/.icons/
-    cp -r $theme10source ~/.icons/
-    cp -r $theme11source ~/.icons/
-    cp -r $theme12source ~/.icons/
-    cp -r $theme13source ~/.icons/
-    cp -r $theme14source ~/.icons/
 else
     mkdir /usr/share/icons
     cp -r $theme1source /usr/share/icons/
     cp -r $theme2source /usr/share/icons/
     cp -r $theme3source /usr/share/icons/
-    cp -r $theme4source /usr/share/icons/
-    cp -r $theme5source /usr/share/icons/
-    cp -r $theme6source /usr/share/icons/
-    cp -r $theme7source /usr/share/icons/
-    cp -r $theme8source /usr/share/icons/
-    cp -r $theme9source /usr/share/icons/
-    cp -r $theme10source /usr/share/icons/
-    cp -r $theme11source /usr/share/icons/
-    cp -r $theme12source /usr/share/icons/
-    cp -r $theme13source /usr/share/icons/
-    cp -r $theme14source /usr/share/icons/
 fi
 
 # Copy XFCE specific icons
@@ -114,10 +70,10 @@ if [ "$xfce" == "y" ]
   then
     if [ "$UID" -ne "$ROOT_UID" ]
       then
-      cp -r $statuspath ~/.icons/Kawaiki-Only/
+      cp -r $statuspath ~/.icons/KawaikiNX-Only/
     else
       mkdir /usr/share/icons
-      cp -r $statuspath /usr/share/icons/Kawaiki-Only/
+      cp -r $statuspath /usr/share/icons/KawaikiNX-Only/
     fi
 fi
 
@@ -147,10 +103,10 @@ themeindexsrc=$basedir$themeindex
 
 if [ "$UID" -ne "$ROOT_UID" ]
 then
-    cp $themeindexsrc ~/.icons/Kawaiki-Only/
+    cp $themeindexsrc ~/.icons/KawaikiNX-Only/
 else
     mkdir /usr/share/icons
-    cp $themeindexsrc /usr/share/icons/Kawaiki-Only/
+    cp $themeindexsrc /usr/share/icons/KawaikiNX-Only/
 fi
 
 
@@ -178,14 +134,14 @@ brightnesssrc=$basedir$brightness
 
 if [ "$UID" -ne "$ROOT_UID" ]
 then
-    cp -r $brightnesssrc ~/.icons/Kawaiki-Only/
+    cp -r $brightnesssrc ~/.icons/KawaikiNX-Only/
 else
     mkdir /usr/share/icons
-    cp -r $brightnesssrc /usr/share/icons/Kawaiki-Only/
+    cp -r $brightnesssrc /usr/share/icons/KawaikiNX-Only/
 fi
 
 
-echo "You can use Gnome/Unity Tweak Tool, to choose Kawaiki-Icon-Theme."
+echo "You can use Gnome/Unity Tweak Tool, to choose KawaikiNX-Icon-Theme."
 echo "Thanks for installing! Please consider donating to this project."
 
 exit
